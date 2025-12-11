3c83b5d8a1d0fe3c8c1222826244e1e34d196658
achmadakif <83754562+AchmadAkif@users.noreply.github.com>
2025-12-11 08:30:36 +0700

    feat: load and print states of Oven instance from the MicrowaveOven metamodel

--- main.py
+++ main.py
@@ -1,4 +1,23 @@
-import bridgepoint
+import os
+from bridgepoint import ooaofooa
+from xtuml import navigate_one as one
+from xtuml import navigate_many as many
 
-metamodel = bridgepoint.load_metamodel('MicrowaveOven.xtuml')
-print(metamodel)
+m = ooaofooa.load_metamodel('MicrowaveOven.xtuml')
+
+## GET ALL CLASS INSTANCES FROM METAMODEL
+# modelClassInstances = m.select_many('O_OBJ')
+
+# for clazz in modelClassInstances:
+# 	print(clazz.Name) 
+
+## GET INSTANCE STATE
+OvenInstance = m.select_one('O_OBJ', lambda sel: sel.name == 'Oven')
+OvenInstanceSM = one(OvenInstance).SM_ISM[518]()
+OvenSM = one(OvenInstanceSM).SM_SM[517]()
+OvenStates = many(OvenSM).SM_STATE[501]()
+
+for state in OvenStates:
+	print(state.Name)
+    
+
