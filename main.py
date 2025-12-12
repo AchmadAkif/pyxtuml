9f44bfadf79223de0da7bfe4f676e8d16132b0e2
achmadakif <83754562+AchmadAkif@users.noreply.github.com>
2025-12-12 07:55:40 +0700

    feat: instance retrieval function

--- main.py
+++ main.py
@@ -1,23 +1,38 @@
-import os
 from bridgepoint import ooaofooa
 from xtuml import navigate_one as one
 from xtuml import navigate_many as many
 
-m = ooaofooa.load_metamodel('MicrowaveOven.xtuml')
+m = ooaofooa.load_metamodel('Tracking.xtuml')
 
 ## GET ALL CLASS INSTANCES FROM METAMODEL
-# modelClassInstances = m.select_many('O_OBJ')
+def getAllClassInstances(kind: str): 
+	classInstancesList = m.select_many(kind)
+	
+	return classInstancesList 
+ 
+## GET INSTANCE STATE MACHINE
+def getInstanceStateMachine(kind: str, args):
+	instance = m.select_one(kind, args)
+	instanceSM = one(instance).SM_ISM[518]()
+	stateMachine = one(instanceSM).SM_SM[517]()
 
-# for clazz in modelClassInstances:
-# 	print(clazz.Name) 
+	return stateMachine
 
-## GET INSTANCE STATE
-OvenInstance = m.select_one('O_OBJ', lambda sel: sel.name == 'Oven')
-OvenInstanceSM = one(OvenInstance).SM_ISM[518]()
-OvenSM = one(OvenInstanceSM).SM_SM[517]()
-OvenStates = many(OvenSM).SM_STATE[501]()
+## GET INSTANCE STATES
+def getInstanceStates(kind: str, args):
+	stateMachine = getInstanceStateMachine(kind, args)
+	statesList = many(stateMachine).SM_STATE[501]()
 
-for state in OvenStates:
-	print(state.Name)
+	return statesList
+
+## GET INSTANCE STATE TRANSITIONS
+def getInstanceStateTransitions(kind: str, args):
+	statesList = getInstanceStates(kind, args)
+	transitionsList = many(statesList).SM_TXN[506]()
+
+	return transitionsList 
     
 
+WoSesh = m.select_one('O_OBJ', lambda sel: sel.name == 'WorkoutSession')
+for o_tfr in many(WoSesh).O_TFR[115]():
+	print(o_tfr.Action_Semantics_internal)
